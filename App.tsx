6339870a91b72681700c414d97c07319429812c9
jaegeunha94 <jaegeunha94@gmail.com>
2022-08-02 15:01:19 +0900

    Add useImperativeHandle hook test source

--- App.tsx
+++ App.tsx
@@ -2,16 +2,14 @@ import * as React from 'react';
 import './style.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './src/Home';
-import UseContext from './src/Context/ReactContext/UseContext';
-import ContextType from './src/Context/ReactContext/ContextType';
+import UseImperativeHandle from './src/Hook/UseImperativeHandle';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/useContext" element={<UseContext />} />
-        <Route path="/contextType" element={<ContextType />} />
+        <Route path="/useimperative" element={<UseImperativeHandle />} />
       </Routes>
     </Router>
   );
