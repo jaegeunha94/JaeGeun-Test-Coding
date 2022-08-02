7f5133681896b8cce38cca9b4c86a152c982bd7e
jaegeunha94 <jaegeunha94@gmail.com>
2022-08-02 14:30:35 +0900

    Add React Context

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import './style.css';
-import Home from './src/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './src/Home';
+import UseContext from './src/Context/ReactContext/UseContext';
+import ContextType from './src/Context/ReactContext/ContextType';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/useContext" element={<UseContext />} />
+        <Route path="/contextType" element={<ContextType />} />
       </Routes>
     </Router>
   );
