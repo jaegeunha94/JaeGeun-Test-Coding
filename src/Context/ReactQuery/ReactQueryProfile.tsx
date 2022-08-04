99614f58f6ce117fabc56b5f04c9175b50e2f57d
jaegeunha94 <jaegeunha94@gmail.com>
2022-08-04 11:31:57 +0900

    Update React Query TestCode

--- src/Context/ReactQuery/ReactQueryProfile.tsx
+++ src/Context/ReactQuery/ReactQueryProfile.tsx
@@ -3,7 +3,20 @@ import { useQuery } from 'react-query';
 import Profile from '../../Context/ReactQuery/Profile';
 
 const ReactQueryProfile = () => {
-  const { isLoading, error, data, isFetching } = useQuery(['/posts']);
+  let userId;
+
+  setTimeout(() => {
+    userId = 1;
+  }, 0);
+
+  const { isLoading, error, data, isFetching } = useQuery(
+    ['/posts'],
+    () =>
+      fetch('https://61b88c9d64e4a10017d19053.mockapi.io/user').then((res) =>
+        res.json()
+      ),
+    { enabled: !!userId }
+  );
 
   if (error) return <div>{error}</div>;
   if (isLoading) return <div>loading...</div>;
