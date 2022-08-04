a66d74c230999bf893bbe48971a1b37480415b0b
jaegeunha94 <jaegeunha94@gmail.com>
2022-08-04 14:43:13 +0900

    ADD nested routing in react-router-dom

--- App.tsx
+++ App.tsx
@@ -2,12 +2,20 @@ import * as React from 'react';
 import './style.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReactQueryProfile from './src/Context/ReactQuery/ReactQueryProfile';
+import Home from './src/Home';
+import NestedTest2 from './src/ReactRouterDOM/NestedTest2';
+import NestedTest from './src/ReactRouterDOM/NestedTest';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ReactQueryProfile />} />
+        <Route path="/home" element={<Home />} />
+        //중첩 라우팅
+        <Route path="/nest" element={<NestedTest />}>
+          <Route path=":id" element={<NestedTest2 />} />
+        </Route>
       </Routes>
     </Router>
   );
