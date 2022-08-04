a241fced7b8839e6c5c222f492bbe4efb8893aca
jaegeunha94 <jaegeunha94@gmail.com>
2022-08-04 15:17:34 +0900

    Add Suspense test code

--- App.tsx
+++ App.tsx
@@ -1,25 +1,16 @@
 import * as React from 'react';
 import './style.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ReactQueryProfile from './src/Context/ReactQuery/ReactQueryProfile';
-import Home from './src/Home';
-import ServeRouting from './src/ReactRouterDOM/ServeRouting/ServeRouting';
-import RouteHook from './src/ReactRouterDOM/RouteHook/RouteHook';
+import SuspenseTest from './src/Suspense/SuspenseTest';
+import Spinner from './src/Suspense/Spinner';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ReactQueryProfile />} />
-        <Route path="/home" element={<Home />} />
-        {/* // Route hook */}
-        <Route path="/hooks/:id" element={<RouteHook />} />
-        {/* // 서브 라우팅 */}
-        <Route path="/serve/*" element={<ServeRouting />} />
-        {/* //중첩 라우팅
-        <Route path="/nest" element={<NestedTest />}>
-          <Route path=":id" element={<NestedTest2 />} />
-        </Route>  */}
+        <React.Suspense fallback={<Spinner />}>
+          <Route path="/" element={<SuspenseTest />} />
+        </React.Suspense>
       </Routes>
     </Router>
   );
